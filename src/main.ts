96a2f0bbfd36346ed32c37f9c3815c2bb375fe6b
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-04-18 16:40:27 +0200

    Enable Debug Tools

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableDebugTools } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
@@ -10,4 +11,9 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then((module) => {
+    const appRef = module.injector.get(ApplicationRef);
+    const appComponent = appRef.components[0];
+    enableDebugTools(appComponent);
+  })
   .catch((err) => console.error(err));
